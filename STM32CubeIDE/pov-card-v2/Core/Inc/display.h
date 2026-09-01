44efd04704ce6cb4c2c12705b18b9aad683bea34
Excessive Overkill <46392388+ExcessiveOverkill@users.noreply.github.com>
2026-09-01 16:26:57 -0400

    remove dead code

--- STM32CubeIDE/pov-card-v2/Core/Inc/display.h
+++
@@ -1,8 +0,0 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
-
-#include <stdint.h>
-
-void set_column(uint8_t* col_data);
-
-#endif
--- STM32CubeIDE/pov-card-v2/Core/Src/display.c
+++
@@ -1,5 +0,0 @@
-#include "display.h"
-
-void set_column(uint8_t* col_data) {
-
-}
